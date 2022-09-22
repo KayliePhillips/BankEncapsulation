ebce32401c78608b7249e07872b950a3c0795dc7
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-22 15:54:10 -0500

    initial commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using BankEncapsulation;
+
+var bankAccountKaylie = new BankAccount();
+
+Console.Write("Please enter how much you would like to deposit: $ ");
+var userDeposit = double.Parse(Console.ReadLine());
+
+bankAccountKaylie.Deposit(userDeposit);
+
+double answer = bankAccountKaylie.GetBalance();
+
+Console.WriteLine($"Your new balance is $ {answer}");
+
+//Can write this either way. 
+//Console.WriteLine($"Your new balance is {bankAccountKaylie.GetBalance()}");
+
+
